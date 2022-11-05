6b6e4eb943779991796f95d18b42d86ef04e8fe3
Babatunde Alli <babatunde.alli@mail.mcgill.ca>
2022-11-05 17:15:35 -0400

    Update ruby_and_activerecord_installer.sh

--- ruby_and_activerecord_installer.sh
+++ ruby_and_activerecord_installer.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+sudo yum update -y
 curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
 \curl -sSL https://get.rvm.io | bash -s stable
 source /home/ec2-user/.rvm/scripts/rvm
 echo `rvm --version` is now installed
 rvm install 2.6.3
 echo `ruby --version` is now installed
-sudo yum install postgresql-devel
+sudo yum install postgresql-devel -y
 gem install activerecord -v 5.2.3
