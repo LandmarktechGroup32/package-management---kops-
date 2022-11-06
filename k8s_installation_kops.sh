8c00ea4da53701a7aa7c6aad41a67b750e816650
Babatunde Alli <babatunde.alli@mail.mcgill.ca>
2022-11-06 14:01:29 -0500

    Create k8s_installation_kops.sh

---
+++ k8s_installation_kops.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install unzip -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+#install kops
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
+
+aws s3 mb s3://shifu.k8s.local
+echo export NAME=shifu.k8s.local >> .bashrc
+echo export KOPS_STATE_STORE=s3://shifu.k8s.local >> .bashrc
+source .bashrc
+
+yes '' | ssh-keygen -N ''
+
+kops create cluster --zones ca-central-1a --networking calico --master-size t3.medium --master-count 1 --node-size t3.medium --node-count=2 ${NAME}
+kops create secret --name ${NAME} sshpublickey admin -i ~/.ssh/id_rsa.pub
+kops update cluster --name shifu.k8s.local --yes --admin
+kops validate cluster
+kubectl get nodes
+
+# kops delete cluster --name=${NAME} --state=${KOPS_STATE_STORE} --yes 
